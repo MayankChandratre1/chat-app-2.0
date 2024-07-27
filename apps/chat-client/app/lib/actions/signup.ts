d1a462b7c4bdfa14a7210b403aaa738ccbc7ca72
Mayank <mayankmchandratre@gmail.com>
2024-07-27 23:14:42 +0530

    lib

---
+++ apps/chat-client/app/lib/actions/signup.ts
@@ -0,0 +1,15 @@
+'use server'
+
+
+type signupProps = {
+    username: string,
+    phone: string,
+    email: string,
+    password: string
+}
+
+const signup = async (data:signupProps) =>{
+    
+}
+
+export default signup
