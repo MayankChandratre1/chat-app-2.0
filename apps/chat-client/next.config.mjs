634106580b62d55ccb1a47516bcb76e2c46ecfb6
Mayank <mayankmchandratre@gmail.com>
2024-08-02 00:39:41 +0530

    test-deploy-6

--- apps/chat-client/next.config.mjs
+++ apps/chat-client/next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
         DATABASE_URL: process.env.DATABASE_URL,
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        WEBSOCKET_URL: process.env.WEBSOCKET_URL,
       }
 };
 
