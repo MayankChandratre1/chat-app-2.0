b1deddf983daaa0d5a0c39f82b09de240f593c9a
Mayank <mayankmchandratre@gmail.com>
2024-08-01 21:41:10 +0530

    environment var update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 echo "DATABASE_URL=${DATABASE_URL}" > /usr/app/packages/db/.env
+echo "NEXTAUTH_URL=${NEXTAUTH_URL}\nNEXTAUTH_SECRET=${NEXTAUTH_SECRET}" > /usr/app/apps/chat-client/.env
+
 npm run db:migrate
 npm run db:generate
 npm run build
